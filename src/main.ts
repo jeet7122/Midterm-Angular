fad0fcc0b6c47d8f189b99b94d3ea0f0b8f9064f
Jeet Chintan Thakkar <w0807360@myscc.ca>
2024-10-17 16:25:19 -0400

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,31 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from '@angular/router';
+import {AccountListComponent} from './app/account-list/account-list.component';
+import {AccountDetailsComponent} from './app/account-details/account-details.component';
+import {PageNotFoundComponent} from './app/page-not-found/page-not-found.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes:Routes = [  {path: "", redirectTo:'/accounts', pathMatch: "full"},
+                {path: 'accounts', component: AccountListComponent},
+                {path: 'accounts/:id', component: AccountDetailsComponent},
+                {path: '**', component: PageNotFoundComponent}];
+
+
+bootstrapApplication(AppComponent,{
+  providers: [provideRouter(routes)]}).then(re => console.log('Bootstrap successful'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
